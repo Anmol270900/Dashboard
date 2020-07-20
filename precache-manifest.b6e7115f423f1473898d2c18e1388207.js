7d7af11e5bccb3ff43ad03d3766ce289ed374bd1
Anmol <anmol270900@gmail.com>
2020-07-20 22:51:24 +0530

    Updates

---
+++ precache-manifest.b6e7115f423f1473898d2c18e1388207.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f3dfbfceff5be0de5e488873fed91eb",
+    "url": "/Dashboard/index.html"
+  },
+  {
+    "revision": "cec4768fdfd96a2e3cea",
+    "url": "/Dashboard/static/css/main.0bc4455c.chunk.css"
+  },
+  {
+    "revision": "53b6c5709ef1b6d82425",
+    "url": "/Dashboard/static/js/2.6e6cf657.chunk.js"
+  },
+  {
+    "revision": "cec4768fdfd96a2e3cea",
+    "url": "/Dashboard/static/js/main.515ccce8.chunk.js"
+  },
+  {
+    "revision": "d8bc9ec02fd6e05b6a8d",
+    "url": "/Dashboard/static/js/runtime~main.8d25dc92.js"
+  }
+]);
